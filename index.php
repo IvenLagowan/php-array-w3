a5b3055e90a09e2b49bfe34b85516d4f83ebc34f
Your Name <you@example.com>
2023-03-27 20:01:58 -0700

    update file arry

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$car=array("Volvo","BMW","Toyota");
+$arrlength=count($car);
+
+for($x=0;$x<$arrlength;$x++)
+  {
+  echo $car[$x];
+  echo "<br>";
+  }
+?>
+
+</body>
+</html>
